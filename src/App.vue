0a29e5c96614aa8501cc275c30fb47a7554da7f0
Madhura Tayade <tayademadhura02@gmail.com>
2025-08-15 00:05:27 +0530

    complete-todo-app-with-add-edit-delete-functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,112 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from 'vue';
+import TodoList from './TodoList.vue';
+
+const todos = ref([]);
+const index = ref(0)
+const text = ref('')
+const isEdit = ref(false)
+const editId = ref(null)
+
+const addToDo = () => {
+  if (isEdit.value && editId.value) {
+    const todo = todos.value.find(x => x.id === editId.value)
+    if (todo) {
+      todo.name = text.value
+    }
+    isEdit.value = false
+    editId.value = 0
+  } else {
+    todos.value.push({ id: ++index.value, name: text.value })
+  }
+  // reset input box value
+  text.value = ''
+}
+
+const deleteToDo = (id) => {
+  const list = todos.value.filter(x => x.id !== id)
+  todos.value = list;
+}
+
+const editToDo = (id) => {
+  isEdit.value = true
+  editId.value = id
+  const todo = todos.value.find(x => x.id === id)
+  if (todo) text.value = todo.name
+}
+
+const btnString = computed(() => {
+  return !isEdit.value ? 'Add' : 'Edit'
+})
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>ToDo List</h1>
+  <div class="main-container">
+    <div class="top-container">
+      <label for="text">Your input:</label>
+      <input id="text" type="text" v-model="text" />
+      <button :disabled="text == ''" @click="addToDo">{{ btnString }}</button>
+    </div>
+    <div class="bottom-container">
+      <TodoList :todos="todos" @delete-todo="deleteToDo" @edit-todo="editToDo"></TodoList>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  font-size: 24px;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  text-transform: uppercase;
+  letter-spacing: 1.9px;
+  font-size: 50px;
+  margin: 50px auto;
+}
+
+.main-container {
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.top-container {
+  display: flex;
+  gap: 15px;
+}
+
+button {
+  margin-right: 20px;
+  padding: 10px 20px;
+  border: none;
+  background-color: rgb(25, 118, 210, 0.8);
+  color: white;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 20px;
+  text-transform: uppercase;
+}
+
+input {
+  padding: 10px;
+  outline: none;
+  border: none;
+  border-bottom: 2px solid green;
+  width: 400px;
+  position: center;
+}
+
+.bottom-container {
+  margin-top: 50px;
+  min-width: 600px;
+}
+</style>
---
+++ src/TodoList.vue
@@ -0,0 +1,103 @@
+<script setup>
+const todos = defineProps({
+    todos: {
+        type: Array,
+        required: true
+    }
+})
+const emit = defineEmits(['delete-todo', 'edit-todo'])
+const handleDelete = (id) => {
+    emit('delete-todo', id)
+}
+const handleEdit = (id) => {
+    emit('edit-todo', id)
+}
+</script>
+
+<template>
+    <div class="todo-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody v-for="(todo, index) in todos.todos" :key="index">
+                <tr>
+                    <td>{{ todo.id }}</td>
+                    <td>{{ todo.name }}</td>
+                    <td>
+                        <button @click="() => handleEdit(todo.id)">Edit</button>
+                        <button @click="() => handleDelete(todo.id)">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+.todo-container {
+    width: 100%;
+    margin: 0 auto;
+    border: 2px solid #333;
+    border-radius: 4px;
+    overflow: hidden;
+    font-family: sans-serif;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    padding: 0 30px;
+}
+
+thead {
+    background-color: #333;
+    color: white;
+}
+
+th,
+td {
+    padding: 12px;
+    text-align: left;
+    border-bottom: 1px solid #ccc;
+}
+
+th:last-child {
+    padding-right: 60px;
+}
+
+/* Right-align the last column (Actions) */
+th:last-child,
+td:last-child {
+    text-align: right;
+}
+
+th:first-child,
+td:first-child {
+    padding-left: 20px;
+}
+
+tbody tr:nth-child(even) {
+    background-color: #f2f2f2;
+}
+
+button {
+    margin-right: 20px;
+    padding: 4px 10px;
+    border: none;
+    background-color: rgb(25, 118, 210, 0.8);
+    color: white;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 20px;
+    text-transform: uppercase;
+}
+
+button:last-child {
+    background-color: rgb(211, 47, 47, 0.8);
+}
+</style>
